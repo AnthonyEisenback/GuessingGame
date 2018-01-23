96020f7f10cba349e8c6317f02b9d83184524058
AnthonyEisenback <anthonyeisenback@Interapts-MacBook-Pro-3.local>
2018-01-23 16:21:11 -0500

    Allowed and prompted for user input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /*
+        1.allow user input
+        2.tell the user if their guess is too high or too low.
+        3. allow for 5 chances, if the number isn't correct tell them what it is
+        4. if the number is correct, congratulate them
+        5. Allow the user to play again if they would like.
+          */
+
+        System.out.println("welcome player, What is your name?");
+
+        Scanner input = new Scanner(System.in);
+
+        int userGuess = input.nextLine();
+
+        
+
+
+
+
     }
 }
