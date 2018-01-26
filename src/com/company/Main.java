7332e8f37c9ea070d7a345b4b4deee07a8b7f139
AnthonyEisenback <anthonyeisenback@Interapts-MacBook-Pro-3.local>
2018-01-26 15:27:20 -0500

    added loops and if statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,16 +13,48 @@ public class Main {
         5. Allow the user to play again if they would like.
           */
 
-        System.out.println("welcome player, What is your name?");
+        System.out.println("welcome player to a number Guessing Game, What is your name?");
 
-        Scanner input = new Scanner(System.in);
-
-        int userGuess = input.nextLine();
-
-        
+        Scanner name = new Scanner(System.in);
+        String myName = name.nextLine();
+        String welcoming = "Welcome, ";
 
 
+        System.out.println(welcoming + myName);
+
+//        asks your name and your guess and checks to see what you inputted.
 
 
+        Scanner myGuess = new Scanner(System.in);
+        int numberOfGuesses =0;
+
+        final int MAX_GUESS_COUNT = 5;
+
+        System.out.println("Enter your guess:");
+        int guess = myGuess.nextInt();
+
+        int randomNum = (int) (1 + Math.random() * 100);
+
+        if (guess == randomNum) {
+
+            System.out.println("You guessed the number.");
+        }
+
+
+        do {
+            if (guess > randomNum) {
+
+                System.out.println("your number is too high!");
+                numberOfGuesses++;
+            } else if (guess < randomNum) {
+                System.out.println("Your number is too low!");
+                numberOfGuesses++;
+            }
+
+            else if (numberOfGuesses == MAX_GUESS_COUNT){
+                System.out.println("Sorry you have ran out of guesses. The correct number was: " + randomNum);
+            }
+
+        } while(numberOfGuesses <= 5) ;
     }
 }
